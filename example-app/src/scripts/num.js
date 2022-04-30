a350e1935ee2add60dc693e69cb6b217ff8d5952
Blaise Antonio I <antonio.iradukunda@gmail.com>
2022-04-30 06:49:42 +0200

    modification structures des projets

--- example-app/src/scripts/num.js
+++
@@ -1,9 +0,0 @@
-/*
-  exemple trivial d'export de constantes
-*/
-
-const PI = 3.1415;
-const ZERO = 0;
-
-// export multiple
-export {PI, ZERO}
--- template-for-app/src/scripts/main.js
+++
@@ -1,5 +0,0 @@
-// write your code here
-console.log('le bundle a été généré !');
-console.log('this is amazing grace');
-
-
